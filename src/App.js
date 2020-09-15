46162bef9e003c85b0e45c0e8149729802ee45ec
zakariabijoy <zakaria@vonome.com>
2020-09-15 21:23:09 +0600

    Connecting Component Using react-redux

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 import Bugs from './components/bugs';
 import configureStore from './store/configureStore';
-import StoreContext from './contexts/storeContext';
+import { Provider } from 'react-redux';
+
 
 const store = configureStore();
 
 function App() {
   return (
-    <StoreContext.Provider value={store}>
+    <Provider store={store}>
       <Bugs />
-    </StoreContext.Provider>
+    </Provider>
 
   );
 }
--- src/components/bugs.jsx
+++ src/components/bugs.jsx
@@ -1,31 +1,16 @@
 import React, { Component } from "react";
-import StoreContext from "./../contexts/storeContext";
+import { connect } from "react-redux";
 import { loadBugs } from "./../store/bugs";
 
 class Bugs extends Component {
-  static contextType = StoreContext;
-
-  state = { bugs: [] };
-
   componentDidMount() {
-    const store = this.context;
-
-    this.unsubscribe = store.subscribe(() => {
-      const bugsInStore = store.getState().entities.bugs.list;
-      if (this.state.bugs !== bugsInStore) this.setState({ bugs: bugsInStore });
-    });
-
-    store.dispatch(loadBugs());
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
+    this.props.loadBugs();
   }
 
   render() {
     return (
       <ul>
-        {this.state.bugs.map((bug) => (
+        {this.props.bugs.map((bug) => (
           <li key={bug.id}>{bug.description}</li>
         ))}
       </ul>
@@ -34,5 +19,14 @@ class Bugs extends Component {
 }
 
 //Bugs.contextType = StoreContext;
+const mapStateToProps = (state) => ({
+  bugs: state.entities.bugs.list,
+});
 
-export default Bugs;
+const mapDispatchToProps = (dispatch) => ({
+  loadBugs: () => dispatch(loadBugs()),
+});
+
+// container
+// presentation (Bugs)
+export default connect(mapStateToProps, mapDispatchToProps)(Bugs);
